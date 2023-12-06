3f69b6b02eb8e285ff8a5154f574b05196ef1adf
samiesmilz <samiesmilz@gmail.com>
2023-12-07 02:29:27 +0800

    Create cupcake model

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 """Flask app for Cupcakes"""
+import requests
+from models import db_connect, Cupcake, db
+from flask import Flask, redirect, render_template, request
+
+
+app = Flask(__name__, static_folder="static")
+
+# Configure app information
+app.config['SECRET_KEY'] = 'devwoof0701'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+# connect to db
+db_connect(app)
--- models.py
+++ models.py
@@ -1,1 +1,24 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+# create database
+db = SQLAlchemy()
+
+
+def db_connect(app):
+    db.app = app
+    db.init_app(app)
+
+# create the cupcake model
+
+
+class Cupcake(db.Model):
+    __tablename__ = 'cupcakes'
+
+    # set up model attributes to turn into table columns
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False,
+                      default="https://tinyurl.com/demo-cupcake")
